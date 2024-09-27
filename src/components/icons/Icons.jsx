5b8af185d8e873ca5ebb13ec5f81e5b6a7c20924
Martin Jose Garcia Muñoz <martinillora@gmail.com>
2024-09-27 14:20:35 +0200

    feature/improvementsUXUI
    Added new features to improve UX/UI

---
+++ src/components/icons/Icons.jsx
@@ -0,0 +1,9 @@
+import { IoPersonOutline, IoBriefcaseOutline, IoReaderOutline, IoLibraryOutline } from "react-icons/io5";
+
+export const PersonIcon = () => <IoPersonOutline />;
+
+export const BriefcaseIcon = () => <IoBriefcaseOutline />;
+
+export const ReaderIcon = () => <IoReaderOutline />;
+
+export const LibraryIcon = () => <IoLibraryOutline />;
---
+++ src/constants/icons.js
@@ -0,0 +1,18 @@
+const icons = [
+  { name: 'Flutter', class: 'devicon-flutter-plain' },
+  { name: 'React', class: 'devicon-react-original' },
+  { name: 'JavaScript', class: 'devicon-javascript-plain' },
+  { name: 'jQuery', class: 'devicon-jquery-plain' },
+  { name: 'TypeScript', class: 'devicon-typescript-plain' },
+  { name: 'HTML5', class: 'devicon-html5-plain' },
+  { name: 'CSS3', class: 'devicon-css3-plain' },
+  { name: 'TailwindCSS', class: 'devicon-tailwindcss-plain' },
+  { name: 'Bootstrap', class: 'devicon-bootstrap-plain' },
+  { name: 'Node.js', class: 'devicon-nodejs-plain' },
+  { name: 'NestJS', class: 'devicon-nestjs-plain' },
+  { name: 'Next.js', class: 'devicon-nextjs-plain' },
+  { name: '.NET', class: 'devicon-dotnetcore-plain' },
+  { name: 'Sass', class: 'devicon-sass-original' },
+];
+
+export default icons;
